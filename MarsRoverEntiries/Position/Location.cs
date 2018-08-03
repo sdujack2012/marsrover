50c9662018d4279fa2b45e6d76181ec3cc8c6f50
TT\kaij <kaij@ticketek.com.au>
2018-08-03 15:08:30 +1000

    add location object

---
+++ MarsRoverEntiries/Position/Location.cs
@@ -0,0 +1,41 @@
+using MarsRover.Interface;
+
+namespace MarsRover.Direction
+{
+    public class Location : ILocation
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Location(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public ILocation MoveXForward()
+        {
+            return new Location(X + 1, Y);
+        }
+
+        public ILocation MoveYForward()
+        {
+            return new Location(X, Y + 1);
+        }
+
+        public ILocation MoveXBackward()
+        {
+            return new Location(X - 1, Y);
+        }
+
+        public ILocation MoveYBackward()
+        {
+            return new Location(X, Y - 1);
+        }
+
+        public override string ToString()
+        {
+            return $"{X} {Y}";
+        }
+    }
+}
